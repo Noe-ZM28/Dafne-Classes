ef8adcd8b93023c79b3c201774315a05307253fd
Noé Zúñiga <noe.zm2000@gmail.com>
2024-05-10 23:12:55 -0600

    ejercicio 1

---
+++ Ejercicios/Ejercicio 1/Solucion 1.py
@@ -0,0 +1,11 @@
+"""
+1) Crear una estructura de datos que te permita guardar tu información personal
+2) Crear una estructura de datos que te permita guardar la estructura anterior junto a la hora del dia
+3) Crear una estructura que guarde 5 estructuras del tipo anterior
+
+4) Modificar la información de la estructura 3
+5) Eliminar la estructura 5
+6) vaciar la estructura inicial
+"""
+
+
