b83ea001170ed7c11c27ad5055e5fa4de7e4a916
NoeZM <noe.zm2000@gmail.com>
2024-06-20 23:03:36 -0600

    se añade clase de salida

--- Modulo 1 - Introducción a python/Clase 10 - Entrada y salida de datos/Salida de datos.py
+++ Modulo 1 - Introducción a python/Clase 10 - Entrada y salida de datos/Salida de datos.py
@@ -1,3 +1,30 @@
-"""
+# Salida de datos
+
 print()
-"""
+
+# Cadena formateada (f-string)
+
+año = 2016
+evento = 'venta'
+print(f'Resultados de la {evento} de {año}')
+# Salida: Resultados de la venta de 2016
+
+
+# Método format()
+
+print('Somos los {} que dicen "{}!"'.format('caballeros', 'Ni'))
+# Salida: Somos los caballeros que dicen "Ni!"
+
+print('{0} y {1}'.format('jamón', 'huevos'))
+# Salida: jamón y huevos
+
+print('{1} y {0}'.format('jamón', 'huevos'))
+# Salida: huevos y jamón
+
+print('Este {comida} está {adjetivo}.'.format(
+      comida='jamón', adjetivo='absolutamente horrible'))
+# Salida: Este jamón está absolutamente horrible.
+
+print('La historia de {0}, {1}, y {otro}.'.format('Juan', 'Manuel',
+                                                   otro='Luis'))
+# Salida: La historia de Juan, Manuel, y Luis.
